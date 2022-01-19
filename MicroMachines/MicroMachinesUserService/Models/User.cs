6a2d4c1b8ae1b1ef5677b1578aa7e6fdc9231a1c
pikuch <55052790+pikuch@users.noreply.github.com>
2022-01-19 20:46:19 +0100

    Fix template leftovers

--- MicroMachines/MicroMachinesUserService/Models/User.cs
+++ MicroMachines/MicroMachinesUserService/Models/User.cs
@@ -3,12 +3,12 @@
     public class User : IUser
     {
         public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public string PhoneNo { get; set; }
-        public string Department { get; set; }
-        public string Address { get; set; }
-        public List<ItineraryItem> Products { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string PhoneNo { get; set; } = null!;
+        public string Department { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public List<ItineraryItem> Products { get; set; } = null!;
     }
 }
