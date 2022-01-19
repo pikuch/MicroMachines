1a671b38ca29187bb03f94b5a4c0ecda87a34f28
pikuch <55052790+pikuch@users.noreply.github.com>
2022-01-19 20:24:01 +0100

    Add data models to account service

---
+++ MicroMachines/MicroMachinesAccountService/Models/Account.cs
@@ -0,0 +1,10 @@
+﻿namespace MicroMachinesAccountService.Models
+{
+    public class Account : IAccount
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public decimal Balance { get; set; }
+        public bool IsClosed { get; set; }
+    }
+}
---
+++ MicroMachines/MicroMachinesAccountService/Models/IAccount.cs
@@ -0,0 +1,10 @@
+﻿namespace MicroMachinesAccountService.Models
+{
+    public interface IAccount
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Balance { get; set; }
+        public bool IsClosed { get; set; }
+    }
+}
