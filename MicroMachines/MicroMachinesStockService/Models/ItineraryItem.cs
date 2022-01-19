5834a0bf3cf21f5ecbf79ea457b3f2167653fb60
pikuch <55052790+pikuch@users.noreply.github.com>
2022-01-19 19:42:56 +0100

    Add data models to stock service

---
+++ MicroMachines/MicroMachinesStockService/Models/ItineraryItem.cs
@@ -0,0 +1,11 @@
+﻿using MicroMachinesCommon.Interfaces;
+
+namespace MicroMachinesStockService.Models
+{
+    public class ItineraryItem : IItineraryItem
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int Count { get; set; }
+    }
+}
