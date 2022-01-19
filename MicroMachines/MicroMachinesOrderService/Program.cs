265591f335d2543758b2d224ba478b877caf371e
pikuch <55052790+pikuch@users.noreply.github.com>
2022-01-19 21:45:06 +0100

    Add db contexts to services

--- MicroMachines/MicroMachinesOrderService/Program.cs
+++ MicroMachines/MicroMachinesOrderService/Program.cs
@@ -1,15 +1,15 @@
+using MicroMachinesOrderService.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<OrderDbContext>(options => options.UseInMemoryDatabase("OrderDatabase"));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
